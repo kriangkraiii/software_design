d03d83d02efe8ecb66b867e2f2182b7c4ea845c4
kriangkraiii <kriangkrai.p@kkumail.com>
2025-06-27 14:40:12 +0700

    Create Users.java

---
+++ Users.java
@@ -0,0 +1,25 @@
+public class Users {
+    private int id;
+    private String name;
+
+    public Users(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
